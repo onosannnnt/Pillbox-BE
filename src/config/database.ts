0542f24330d7a69c197838a2e7edbf846257cd55
หนมจีบ <onopingping@gmail.com>
2024-08-11 18:06:51 +0700

    🐛 fix : adding dotenv config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
 export const port: number = Number(process.env.PORT)
 export const host: string = process.env.HOST
 export const dbPort: number = Number(process.env.DB_PORT)
