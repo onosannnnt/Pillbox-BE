eeca5a4578658875503cb7a0f66ac3a44c415ff7
onosannnnt <onopingping@gmail.com>
2024-10-27 05:44:10 +0700

    fix : forget '/' in router put

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -33,6 +33,6 @@ router.get('/hardwareGetPillChannels/:userID', PillboxController.hardwareGetPill
 router.put('/userTakePill/:channelID', PillboxController.takePill)
 router.get('/getMedicines', PillboxController.getMedicine)
 router.delete('/deletePillChannel/:channelID', PillboxController.deletePillChannel)
-router.put('updatePillChannel/:channelID', PillboxController.updatePillChannel)
+router.put('/updatePillChannel/:channelID', PillboxController.updatePillChannel)
 
 export default router
